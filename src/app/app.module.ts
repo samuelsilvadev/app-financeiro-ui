b50bbfa9c378fc9a2ae884c1452c1262da855a35
samuel silva <samuelsilvawb@gmail.com>
2017-10-14 22:41:02 -0300

    feat(Form Lancamento) : Criar prototipo form de lançamento
    - Adicionado html do form de pesquisa
    - Adicionado css para controlar labels e container geral
    - Importado no modulo principal componentes primeng
    
    Sem issue relacionada
    Samuel Silva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { TabViewModule } from 'primeng/primeng';
+import { ButtonModule } from 'primeng/primeng';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TabViewModule,
+    ButtonModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
