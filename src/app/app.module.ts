53dd9d66f3c9fb8aa12242a0382e60cb60d06d8e
samuel silva <samuelsilvawb@gmail.com>
2017-10-25 05:02:53 -0300

    feat : Criar protótipo inicial da tela de cadastro de lancamento
    - Criado protótipo inicial
    - Adicionado componente no app.module
    
    Samuel Silva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,15 @@ import { AppComponent } from './app.component';
 import { TabViewModule } from 'primeng/primeng';
 import { ButtonModule } from 'primeng/primeng';
 import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
 import { DataTableModule } from 'primeng/primeng';
 import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+
 import { LancamentoSearchComponent } from './lancamento-search/lancamento-search.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MenuLateralComponent } from './navbar/menu-lateral/menu-lateral.component';
 import { PessoaSearchComponent } from './pessoa-search/pessoa-search.component';
+import { LancamentoFormComponent } from './lancamento-form/lancamento-form.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,8 @@ import { PessoaSearchComponent } from './pessoa-search/pessoa-search.component';
     LancamentoSearchComponent,
     NavbarComponent,
     MenuLateralComponent,
-    PessoaSearchComponent
+    PessoaSearchComponent,
+    LancamentoFormComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +31,8 @@ import { PessoaSearchComponent } from './pessoa-search/pessoa-search.component';
     ButtonModule,
     InputTextModule,
     DataTableModule,
-    TooltipModule
+    TooltipModule,
+    InputTextareaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/lancamento-form/lancamento-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lancamento-form',
+  templateUrl: './lancamento-form.component.html',
+  styleUrls: ['./lancamento-form.component.css']
+})
+export class LancamentoFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
